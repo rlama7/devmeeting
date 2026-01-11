8b9f97a5e3ee9ca66d024fe31ca88321b0ff86fe
Ratna Lama <royalray7@gmail.com>
2026-01-10 21:52:50 -0800

    apply initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss.
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferencs, All in One Place.
+      </p>
+    </section>
+  );
+};
+
+export default page;
